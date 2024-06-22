135d893905f880dd570f7cdebcaa3f4ad4f8515c
DEVctr <vferreira487@gmail.com>
2024-06-21 23:15:35 -0300

    Alterando Getters e Setters com Lombok.

--- src/main/java/org/devctr/Cliente.java
+++ src/main/java/org/devctr/Cliente.java
@@ -1,42 +1,17 @@
 package org.devctr;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
     private String nome;
     private String cpf;
     private String email;
     private String telefone;
-
-    public Cliente(String nome, String cpf, String email, String telefone) {}
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 }
--- src/main/java/org/devctr/Conta.java
+++ src/main/java/org/devctr/Conta.java
@@ -1,5 +1,10 @@
 package org.devctr;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class Conta implements IConta {
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 0;
@@ -29,30 +34,6 @@ public abstract class Conta implements IConta {
     public void transferir(double valor, Conta contaDestino) {
         this.sacar(valor);
         contaDestino.depositar(valor);
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
     }
 
     protected void infosConta() {
